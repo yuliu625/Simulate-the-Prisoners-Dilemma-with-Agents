b045f180ef5351fe9ca78e5f234c28dc4c059c36
yu <yul1024@outlook.com>
2025-02-20 20:21:13 +0800

    Create message_protocols.py

---
+++ game/protocols/message_protocols.py
@@ -0,0 +1,40 @@
+"""
+agent之间通讯的协议。
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class GameSetting:
+    system_prompt_template: str
+    inference_prompt_template: str
+
+
+@dataclass
+class ManagerRequest:
+    # system_prompt: str
+    content: str
+    # game_round: int
+
+
+@dataclass
+class ParticipantChoice:
+    content: str
+    result: dict
+    # choice: str
+    # analysis: str
+    # expectation: str
+    # reason: str
+
+
+@dataclass
+class ParsedResult:
+    choice: str
+    analysis: str
+    expectation: str
+    reason: str
+
+
+if __name__ == '__main__':
+    pass
