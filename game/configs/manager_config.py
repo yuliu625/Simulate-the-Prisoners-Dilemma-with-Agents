7318e1784e14fe915f4436eae30a96db22f007fc
yu <yul1024@outlook.com>
2025-02-22 16:27:49 +0800

    Update manager_config.py

--- game/configs/manager_config.py
+++ game/configs/manager_config.py
@@ -2,9 +2,14 @@
 dict形式的试验控制者的设置。
 """
 
-manager_config = dict(
-    description="",
+from autogen_core import AgentId
 
+
+manager_config = dict(
+    description="一场实验的控制者。",
+    participant_ids=[AgentId(type='participant', key='Player1'), AgentId(type='participant', key='Player2'),],
+    game_rules={},
+    game_setting={}
 )
 
 
