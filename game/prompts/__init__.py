4bcfa3d40079e10398721e7611e70f11c7dc4382
yu <yul1024@outlook.com>
2025-02-22 16:27:56 +0800

    Update __init__.py

--- game/prompts/__init__.py
+++ game/prompts/__init__.py
@@ -3,5 +3,15 @@ agent的system prompt，
 给出agent的身份，引导进行选择。
 """
 
-__all__ = []
+__all__ = [
+    'get_participant_system_prompt_template',
+    'get_history_prompt_template',
+    'get_participant_message_prompt_template'
+]
+
+from .participant_prompts import (
+    get_participant_system_prompt_template,
+    get_history_prompt_template,
+    get_participant_message_prompt_template,
+)
 
