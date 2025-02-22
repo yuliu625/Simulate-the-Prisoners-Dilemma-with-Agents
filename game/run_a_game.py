d13cad37633fd51e4b0d991ba7835a8b1de5465b
yu <yul1024@outlook.com>
2025-02-22 17:51:27 +0800

    Update run_a_game.py

--- game/run_a_game.py
+++ game/run_a_game.py
@@ -2,11 +2,28 @@
 根据设置，运行一次试验。
 """
 
-from autogen_core import SingleThreadedAgentRuntime
+from game.runtime import GameRunner
+from game.prompts import (
+    get_participant_system_prompt_template,
+    get_history_prompt_template,
+    get_participant_message_prompt_template,
+)
+from game.protocols import GameRequest
 
 
-def main():
-    ...
+async def run_game():
+    game_runner = GameRunner(
+        manager_config={},
+        participant_config={}
+    )
+
+    await game_runner.run_a_game(
+        GameRequest(
+            game_round=1,
+            participant_message_template="",
+            history_prompt_template=""
+        )
+    )
 
 
 if __name__ == '__main__':
