becc1b201feee9c985e39f71276c6cda237b245d
yu <yul1024@outlook.com>
2025-02-23 14:53:57 +0800

    Update run_a_game.py

--- game/run_a_game.py
+++ game/run_a_game.py
@@ -4,27 +4,37 @@
 
 from game.runtime import GameRunner
 from game.prompts import (
-    get_participant_system_prompt_template,
     get_history_prompt_template,
     get_participant_message_prompt_template,
 )
 from game.protocols import GameRequest
+from game.configs import get_manager_config, get_participant_config
+
+import asyncio
 
 
-async def run_game():
+async def run_game(game_round: int = 1):
+    """
+    运行实验。
+    设置相关的内容需要到configs中进行修改。
+    """
     game_runner = GameRunner(
-        manager_config={},
-        participant_config={}
+        manager_config=get_manager_config(),
+        participant_config=get_participant_config(),
     )
 
     await game_runner.run_a_game(
         GameRequest(
-            game_round=1,
-            participant_message_template="",
-            history_prompt_template=""
+            game_round=game_round,
+            participant_message_template=get_participant_message_prompt_template(
+                participant_message_prompt_template=None
+            ),
+            history_prompt_template=get_history_prompt_template(
+                history_prompt_template=None
+            ),
         )
     )
 
 
 if __name__ == '__main__':
-    pass
+    asyncio.run(run_game())
